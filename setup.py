2c31eef2cd9100f6c3c026307ec8b9b6885f7b7f
StephPortfolioEffect <stephanie.toper@portfolioeffect.com>
2016-10-05 15:10:56 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
 
     description='High Frequency Portfolio Analytics by PortfolioEffect',
     long_description=long_description,
