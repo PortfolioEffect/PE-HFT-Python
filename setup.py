5a3235f67ea2de5716dfe1c5c970aa1f5583a592
StephanieToper <stephanie.toper@portfolioeffect.com>
2017-08-07 21:04:58 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-    version_='1.0.7'
+    version_='1.0.8'
 setup(
     name='hft',
 
